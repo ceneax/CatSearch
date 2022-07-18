3d221321f223f481bb6e55c42a22e861f6db5e23
ceneax <ceneax@gmail.com>
2022-07-18 16:55:53 +0800

    update

--- config/config.go
+++ config/config.go
@@ -26,6 +26,8 @@ var (
 )
 
 const (
+	VERSION = "0.0.1"
+
 	HEADER_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
 	GFW_LIST          = ``
 )
--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"CatSearch/config"
 	"CatSearch/route"
+	"flag"
+	"log"
 )
 
+var debug = flag.Bool("d", false, "Debug 模式")
+
 func main() {
-	config.Debug = false
-	config.Seg.LoadDict("./s_1.txt")
+	flag.Parse()
+	config.Debug = *debug
+
+	err := config.Seg.LoadDict("./s_1.txt")
+	if err != nil {
+		log.Fatal("分词词典加载失败")
+		return
+	}
 
 	go route.StartSearcher()
 	route.StartApi()
